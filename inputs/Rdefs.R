92b98fd4e1a2807f0a654ffbed4446def4c9e0ac
MGehrke <matthias.gehrke@fom.de>
2024-10-30 10:57:31 +0100

    Erster Aufschlag Random Forest

---
+++ inputs/Rdefs.R
@@ -0,0 +1,17 @@
+# R-Definitionen
+
+# ggplot2-Farbschema
+theme.fom <- theme_classic(22*1.04)
+theme_set(theme.fom)
+
+# Farben definieren
+bgcolRed <- "#FFB9BF"               # rot Hintergrund
+fgcolRed <- "#FF4B5C"               # rot Schrift
+bgcolBlue <- "#B6DAFF"              # blau Hintergrund
+fgcolBlue <- "#8FC7FF"              # blau Schrift
+bgcolGray <- "#E6E6E6"              # grau Hintergrund
+fgcolGray <- "#999999"              # grau Schrift
+fgcolDark <- "#666666"              # dunkelgrau Schrift
+bgcolFOMGreen <- "#BFE5E2"          # FOM grün Hintergrund
+fgcolFOMGreen <- "#00998A"          # FOM grün Schrift
+# bei Änderungen der Farben auch Latex-Definitionen anpassen  
